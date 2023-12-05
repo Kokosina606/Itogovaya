59bfa0c00b2f4f6e44c717ed3be28e3c8d845b0b
Kokosina606 <138290381+Kokosina606@users.noreply.github.com>
2023-12-05 22:46:44 +0300

    Add files via upload
    Добавлен код программы

---
+++ zadanie.cs
@@ -0,0 +1,49 @@
+Console.WriteLine("Введите элементы массива через пробел:");
+string[] inputArray = Console.ReadLine().Split(' ');
+
+Console.WriteLine("Введенный массив:");
+foreach (string item in inputArray)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("Результат:");
+
+string[] resultArray = FilterStrings(inputArray);
+
+foreach (string item in resultArray)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
+string[] FilterStrings(string[] inputArray)
+{
+    int count = 0;
+
+    // Кол-во строк, удовлетворяющих условию
+    foreach (string item in inputArray)
+    {
+        if (item.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    // Новый массив
+    string[] resultArray = new string[count];
+    int index = 0;
+
+    // Новые строки, удовлетворяющие условию
+    foreach (string item in inputArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[index] = item;
+            index++;
+        }
+    }
+
+    return resultArray;
+}
